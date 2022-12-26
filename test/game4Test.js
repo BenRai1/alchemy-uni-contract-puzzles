2cf9c47c1fabebc327c4315e530ea139204b3f7d
BenRai1 <benrainow@gmx.de>
2022-12-26 15:28:41 +0100

    Puzzles solved

--- test/game4Test.js
+++ test/game4Test.js
@@ -1,21 +1,30 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
 
-describe('Game4', function () {
+describe("Game4", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game4');
+    const Game = await ethers.getContractFactory("Game4");
     const game = await Game.deploy();
 
-    return { game };
+    // get an array of all accounts that hardhat sets up for us
+    const accounts = await ethers.getSigners();
+
+    return { game, accounts };
   }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+  it("should be a winner", async function () {
+    const { game, accounts } = await loadFixture(deployContractAndSetVariables);
 
     // nested mappings are rough :}
 
-    await game.win();
+    // set the mapping of account 1 to be connected to account 0
+
+    await game.connect(accounts[0]).write(accounts[1].address);
+
+    // call the win function with account 1 and use account 0 as the functions argument
+
+    await game.connect(accounts[1]).win(accounts[0].address);
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
--- test/game5Test.js
+++ test/game5Test.js
@@ -1,21 +1,39 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
+const { ethers } = require("hardhat");
 
-describe('Game5', function () {
+describe("Game5", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
+    const Game = await ethers.getContractFactory("Game5");
     const game = await Game.deploy();
 
-    return { game };
+    const accounts = await ethers.getSigners();
+
+    return { game, accounts };
   }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+  it("should be a winner", async function () {
+    const { game, accounts } = await loadFixture(deployContractAndSetVariables);
 
-    // good luck
+    // account with ETH that will send some ETH to the newly created account
+    const oldAccount = accounts[0];
+    // placeholder for the amount of ETH the old account will send to the new acccount
+    const value = ethers.utils.parseEther("1.0");
+    let found = false;
 
-    await game.win();
+    // creatting new accounts until one account matches the criteria to win
+    while (!found) {
+      // creat new account
+      const wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+      //check if the account address matches the condition to win (starts with 0x00")
+      if (wallet.address.substring(2, 4) == "00") {
+        const to = wallet.address;
+        await oldAccount.sendTransaction({ to, value });
+        await game.connect(wallet).win();
+        found = true;
+      }
+    }
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
